9c9e5380ec4ada4170f6843de22769e0b2ef4250
Mr-BTD <david.m.jansson@gmail.com>
2023-07-05 16:59:16 +0200

    comit

---
+++ JavaScript/porject_script.js
@@ -0,0 +1,8 @@
+// ELEMENT ON THE PAGE
+const ROOT = document.documentElement
+const GAME_AREA = document.getElementById("game-area");
+
+// CONSTS and Normal variables
+let GAME_TAG = "game-(NUMBER)"
+
+
